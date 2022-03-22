ba9262630566a3dde4e0dfa13ad6f94f571a79fb
Brooke Wilder <brookewilder19@gmail.com>
2022-03-22 16:01:45 -0400

    start of all projects view

---
+++ client/components/AllProjects.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import Drawer from '@mui/material/Drawer';
+
+
+export default class AllProject extends Component {
+    constructor(){
+        super();
+        this.state={
+        }
+    }
+
+
+    render(){
+        return(
+            <div>
+
+                  <Drawer>
+                  </Drawer>
+              </div>
+        )
+    }
+}
