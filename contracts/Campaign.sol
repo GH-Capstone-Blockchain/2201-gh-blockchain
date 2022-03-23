ad5cb4f11f5879977745e475f444e37ea3e907d2
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-03-23 19:30:11 -0300

    refund function is working now

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -12,21 +12,21 @@ pragma solidity ^0.8.12;
 // }
 contract Campaign {
     enum State {AWAITING_DONATIONS, CAMPAIGN_DATE_PASSED, AWAITING_FUND_WITHDRAWAL, CAMPAIGN_COMPLETE}
-    //add start date
     uint public campaignId;
     uint public scientistId;
     address public projectAddress;
     string public title;
-    uint public goalAmount;
+    uint public goalAmount; //make sure to have the data from the front end be in wei
     uint256 public startDate;
     uint256 public endDate;
     uint public currentTotalDonations;
-    Donation[] public donationsList;
-    struct Donation {
-        // address supporterAddress;
-        uint supporterId;
-        uint donationAmount;
-    }
+    mapping(uint => mapping(address => uint)) public donationsAmount;
+    // Donation[] public donationsList;
+    // struct Donation {
+    //     // address supporterAddress;
+    //     uint supporterId;
+    //     uint amount;
+    // }
     constructor(uint _campaignId, uint _scientistId, address _projectAddress, string memory _title, uint _goalAmount, uint256 _startDate, uint256 _endDate) {
         campaignId = _campaignId;
         scientistId = _scientistId;
@@ -36,17 +36,16 @@ contract Campaign {
         startDate = _startDate;
         endDate = _endDate;
     }
-    //donors can donate to the campaign
     function donate(uint _supporterId) public payable {
         require(block.timestamp >= startDate, 'not started');
         require(block.timestamp <= endDate, 'ended');
-        Donation memory newDonation = Donation(
-            {
-                supporterId: _supporterId,
-                donationAmount: msg.value
-            }
-        );
-        donationsList.push(newDonation);
+        // Donation memory newDonation = Donation(
+        //     {
+        //         supporterId: _supporterId,
+        //         donationAmount: msg.value
+        //     }
+        // );
+        donationsAmount[_supporterId][msg.sender] += msg.value;
         currentTotalDonations += msg.value;
     }
     //funds are allocated according to goal met...check if goal was met, if so, creatorAddress.transfer(address(this).balance), if not, look it up
@@ -56,6 +55,13 @@ contract Campaign {
         require(block.timestamp > endDate, 'not ended');
         require(address (this).balance >= goalAmount, 'sorry, your campaign did not meet its goal');
         payable(projectAddress).transfer(address (this).balance);
+    }
+    function refund(uint _supporterId) external {
+        require(block.timestamp > endDate, 'not ended');
+        require(address (this).balance <= goalAmount, 'the campaign was successful');
+        uint balance = donationsAmount[_supporterId][msg.sender];
+        donationsAmount[_supporterId][msg.sender] = 0;
+        payable (msg.sender).transfer(balance);
     }
     //scientist can cancel campaign
 }
