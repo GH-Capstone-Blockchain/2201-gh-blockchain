c3e42c9f2ea61dfb127b0b167935cf43d0c3043f
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-03-23 17:52:37 -0300

    combined two smart contracts and money can be received and released once goal amount is reached

---
+++ contracts/Campaign.sol
@@ -0,0 +1,65 @@
+/ SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+// contract CampaignFactory{
+//     address[] public deployedCampaigns;
+//     function createCampaign(uint campaignId, uint scientistId, string title, uint goal, uint endDate) public {
+//         address campaignAddress = new Campaign(campaignId, scientistId, title, goal, endDate, msg.sender); //creates and deploys a new contract
+//         deployedCampaigns.push(campaignAddress);
+//     }
+//     function getDeployedCampaigns() public view returns (address[] memory){
+//         return deployedCampaigns;
+//     }
+// }
+contract Campaign {
+    enum State {AWAITING_DONATIONS, CAMPAIGN_DATE_PASSED, AWAITING_FUND_WITHDRAWAL, CAMPAIGN_COMPLETE}
+    //add start date
+    uint public campaignId;
+    uint public scientistId;
+    address public projectAddress;
+    string public title;
+    uint public goalAmount;
+    uint256 public startDate;
+    uint256 public endDate;
+    Donation[] public donationsList;
+    uint public currentTotalDonations = address(this).balance;
+    struct Donation {
+        // address supporterAddress;
+        uint supporterId;
+        uint donationAmount;
+    }
+    constructor(uint _campaignId, uint _scientistId, address _projectAddress, string memory _title, uint _goalAmount, uint256 _startDate, uint256 _endDate) {
+        campaignId = _campaignId;
+        scientistId = _scientistId;
+        projectAddress = _projectAddress;
+        title = _title;
+        goalAmount = _goalAmount;
+        startDate = _startDate;
+        endDate = _endDate;
+    }
+    //donors can donate to the campaign
+    function donate(uint _supporterId) public payable {
+        //require( state whatever )
+        Donation memory newDonation = Donation(
+            {
+                supporterId: _supporterId,
+                donationAmount: msg.value
+            }
+        );
+        donationsList.push(newDonation);
+    }
+    //funds are allocated according to goal met...check if goal was met, if so, creatorAddress.transfer(address(this).balance), if not, look it up
+    function releaseFund() public {
+        ///require( state whatever )
+        //if enddate is later than timestamp when the function is trigger
+        require(address (this).balance >= goalAmount);
+        //if goal is met, only scientist can call this function (require msg.sender == projectAddress)
+        payable(projectAddress).transfer(address (this).balance);
+    }
+    //scientist can cancel campaign
+}
+//look up how to transfer eth on a certain date in solidity
+//maybe use new Date Javascript function to trigger from the front end a function in smart contract that allocates the funds
+
+
+
+
