12a32ca88bc006fadc12110af6859c1d54066b4e
Anna Hippee <anna.hippee@gmail.com>
2022-03-28 12:50:39 -0400

    progress on dashboard so far

---
+++ client/components/ProjectDashboard.js
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from 'react';
+import { Box, TextField, Button } from '@mui/material';
+import { connect } from 'react-redux';
+import { updateProject, fetchProject } from '../store/singleProject';
+import { useParams } from 'react-router-dom';
+
+const ProjectDashboard = (props) => {
+  let params = useParams();
+  let id = parseInt(params.id);
+
+  const [form, setForm] = useState({
+    name: '',
+    description: '',
+    imageUrl: '',
+    videoUrl: '',
+    project_timeline_start: '',
+    project_timeline_end: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await props.fetchProject(id);
+      } catch (error) {
+        console.error('error in fetchData', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleChange = (e) => {
+    let value = e.target.value;
+    if (e.target.type === 'date') value = new Date(value);
+    setForm({ ...form, [e.target.id]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.updateProject(form);
+  };
+  console.log('PROPS', props);
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
+      <div>
+        <TextField
+          required
+          id="name"
+          label="Project Name"
+          onChange={handleChange}
+        />
+        <TextField
+          id="description"
+          label="Project Description"
+          multiline
+          rows={4}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          id="imageUrl"
+          label="Image URL"
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          id="videoUrl"
+          label="Video URL"
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          type="date"
+          id="campaign_timeline_start"
+          label="Campaign Start Date"
+          InputLabelProps={{ shrink: true }}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          type="date"
+          id="campaign_timeline_end"
+          defaultValue={new Date()}
+          label="Campaign End Date"
+          InputLabelProps={{ shrink: true }}
+          onChange={handleChange}
+        />
+      </div>
+      <Button type="submit">Submit</Button>
+    </Box>
+  );
+};
+
+const mapState = (state) => {
+  return {
+    auth: state.auth,
+    project: state.project.project,
+    scientists: state.project.scientists,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    updateProject: (project) => dispatch(updateProject(project)),
+    fetchProject: (projectId) => dispatch(fetchProject(projectId)),
+  };
+};
+
+export default connect(mapState, mapDispatch)(ProjectDashboard);
--- client/store/singleProject.js
+++ client/store/singleProject.js
@@ -3,39 +3,65 @@ import axios from 'axios';
 // action type constants
 
 const FETCH_PROJECT = 'FETCH_PROJECT';
+// const UPDATE_PROJECT = 'UPDATE_PROJECT';
 
 // action creators
 
 const _fetchProject = (project, scientists) => {
-    return {
-        type: FETCH_PROJECT, 
-        project, 
-        scientists
-    }
-}
+  return {
+    type: FETCH_PROJECT,
+    project,
+    scientists,
+  };
+};
+
+// const _updateProject = (project) => {
+//   return {
+//     type: UPDATE_PROJECT,
+//     project,
+//   };
+// };
 
 // thunks
 
 export const fetchProject = (projectId) => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.get(`/api/project/${projectId}`);
-            const scientists = data.scientists;
-            const project = data.project;
-            dispatch(_fetchProject(project, scientists))
-        } catch (error) {
-            console.error('error in fetchProject thunk', error);
-        }
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/project/${projectId}`);
+      const scientists = data.scientists;
+      const project = data.project;
+      dispatch(_fetchProject(project, scientists));
+    } catch (error) {
+      console.error('error in fetchProject thunk', error);
     }
-}
+  };
+};
+
+export const updateProject = (updatedProject) => {
+  return async (dispatch) => {
+    try {
+        const { data } = await axios.put(`api/projects/:${updatedProject.id}`, updatedProject);
+        dispatch(fetchProject(updatedProject.id))
+    } catch (error) {
+      console.error('Error in updateProject thunk', error);
+    }
+  };
+};
 
 // reducer
 
-export default function singleProjectReducer(state = {project: {}, scientists: []}, action) {
-    switch (action.type) {
-        case FETCH_PROJECT:
-            return {...state, project: action.project, scientists: action.scientists};
-        default:
-            return state;
-    }
+export default function singleProjectReducer(
+  state = { project: {}, scientists: [] },
+  action
+) {
+  switch (action.type) {
+    case FETCH_PROJECT:
+      return {
+        ...state,
+        project: action.project,
+        scientists: action.scientists,
+      };
+    default:
+      return state;
+  }
 }
