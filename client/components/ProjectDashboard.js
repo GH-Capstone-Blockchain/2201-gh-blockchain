af3dd59d6a277f3272c78a697c0b738834e12ccd
Anna Hippee <anna.hippee@gmail.com>
2022-03-29 12:37:18 -0400

    project dashboard working

--- client/components/ProjectDashboard.js
+++ client/components/ProjectDashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Grid, Box, TextField, Button, Typography } from '@mui/material';
+import { Grid, Box, TextField, Button, Typography, List, ListItem, ListItemText } from '@mui/material';
 import { connect } from 'react-redux';
 import { updateProject, fetchProject } from '../store/singleProject';
 import { useParams } from 'react-router-dom';
@@ -41,7 +41,6 @@ const ProjectDashboard = (props) => {
   }, []);
 
   const handleChange = (e) => {
-    console.log(form);
     let value = e.target.value;
     if (e.target.type === 'date') value = new Date(value);
     setForm({ ...form, [e.target.id]: value });
@@ -49,12 +48,8 @@ const ProjectDashboard = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log('FORM', form);
-
     props.updateProject(form);
   };
-  console.log('PROPS', props);
   return (
     <Grid
       container
@@ -89,66 +84,128 @@ const ProjectDashboard = (props) => {
             Project Dashboard
           </Typography>
         </Grid>
-
-        <TextField
-          required
-          id="name"
-          label="Project Name"
-          onChange={handleChange}
-          InputLabelProps={{ shrink: true }}
-          defaultValue={props.project.name}
-        />
-
-        <TextField
-          id="description"
-          label="Project Description"
-          multiline
-          rows={4}
-          onChange={handleChange}
-          InputLabelProps={{ shrink: true }}
-          defaultValue={props.project.description}
-        />
-
-        <TextField
-          required
-          id="imageUrl"
-          label="Image URL"
-          onChange={handleChange}
-          InputLabelProps={{ shrink: true }}
-          defaultValue={props.project.imageUrl}
-        />
-
-        <TextField
-          required
-          id="videoUrl"
-          label="Video URL"
-          onChange={handleChange}
-          InputLabelProps={{ shrink: true }}
-          defaultValue={props.project.videoUrl}
-        />
-
-        <TextField
-          required
-          type="date"
-          id="project_timeline_start"
-          label="Project Start Date"
-          defaultValue={new Date(props.project.project_timeline_start)}
-          InputLabelProps={{ shrink: true }}
-          onChange={handleChange}
-        />
-
-        <TextField
-          required
-          type="date"
-          id="project_timeline_end"
-          label="Project End Date"
-          defaultValue={new Date(props.project.project_timeline_end)}
-          InputLabelProps={{ shrink: true }}
-          onChange={handleChange}
-        />
-
-        <Button type="submit">Submit</Button>
+        <Grid item xs={1} />
+        <Grid item xs={12} style={{ maxWidth: '800px' }}>
+          <Grid
+            container
+            // spacing={2}
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              // background: "#051f2e",
+            }}
+          >
+            <Grid item xs={8}>
+              <TextField
+                fullwidth
+                required
+                id="name"
+                label="Project Name"
+                onChange={handleChange}
+                InputLabelProps={{ shrink: true }}
+                defaultValue={props.project.name}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                fullwidth
+                id="description"
+                label="Project Description"
+                multiline
+                rows={4}
+                onChange={handleChange}
+                InputLabelProps={{ shrink: true }}
+                defaultValue={props.project.description}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                required
+                id="imageUrl"
+                label="Image URL"
+                onChange={handleChange}
+                InputLabelProps={{ shrink: true }}
+                defaultValue={props.project.imageUrl}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                required
+                id="videoUrl"
+                label="Video URL"
+                onChange={handleChange}
+                InputLabelProps={{ shrink: true }}
+                defaultValue={props.project.videoUrl}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                required
+                type="date"
+                id="project_timeline_start"
+                label="Project Start Date"
+                defaultValue={new Date(props.project.project_timeline_start)}
+                InputLabelProps={{ shrink: true }}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                required
+                type="date"
+                id="project_timeline_end"
+                label="Project End Date"
+                defaultValue={new Date(props.project.project_timeline_end)}
+                InputLabelProps={{ shrink: true }}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={1} />
+          <Grid item xs={12} marginBottom="50px"></Grid>
+          <Grid
+            container
+            spacing={2}
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              // background: "#051f2e",
+            }}
+          >
+            <Grid item xs={4}>
+              <Button fullwidth type="submit" sx={{ marginBottom: '10%' }}>
+                Submit
+              </Button>
+            </Grid>
+            <Grid item xs={4}>
+              <Button fullwidth type="submit" sx={{ marginBottom: '10%' }}>
+                Release Funds
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
       </Box>
+      <Grid item xs={12} style={{ maxWidth: '800px' }}>
+        <Grid
+          container
+          // spacing={2}
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            // background: "#051f2e",
+          }}
+        >
+          {/* <List>
+            <ListItem>
+              <ListItemText primary={props.project.name}/>
+            </ListItem>
+          </List> */}
+          
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
