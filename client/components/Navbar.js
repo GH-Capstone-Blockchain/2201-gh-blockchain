eea036b91a453d7bce522c38eb3d503ff46484bc
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-03-30 14:46:10 -0300

    added profile img to nav bar and once clicked it send you to the user profile

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -16,105 +16,127 @@ import {
   Grid,
 } from "@mui/material";
 
-const Navbar = ({ handleClick, isLoggedIn, auth }) => (
-  <div>
-    <AppBar position="fixed" elevation={0} style={{ background: "#051f2e" }}>
-      <Grid
-        container
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "flex-end",
-        }}
-      >
-        <Grid item xs={3}>
-          <Grid
-            container
-            sx={{
-              display: "flex",
-              justifyContent: "flex-start",
-              alignItems: "flex-end",
-            }}
-          >
-            <Grid item xs={3}>
-              <Link to="/">
-                <Box
-                  component="img"
-                  sx={{
-                    height: "50px",
-                    margin: "10px",
-                    marginBottom: "4px",
-                  }}
-                  alt="DeSci Funder"
-                  src="/logo.png"
-                />
-              </Link>
-            </Grid>
-            <Grid item xs={9}>
-              <Link to="/">
-                <Typography
-                  color="primary"
-                  variant="h5"
-                  sx={{ fontFamily: "Roboto Condensed", marginBottom: "4px" }}
-                >
-                  De
-                  <span className="main-title-span">Sci </span>Funder
-                </Typography>
-              </Link>
+const Navbar = ({ handleClick, isLoggedIn, auth, profileImg }) => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <div>
+      <AppBar position="fixed" elevation={0} style={{ background: "#051f2e" }}>
+        <Grid
+          container
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "flex-end",
+          }}
+        >
+          <Grid item xs={3}>
+            <Grid
+              container
+              sx={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "flex-end",
+              }}
+            >
+              <Grid item xs={3}>
+                <Link to="/">
+                  <Box
+                    component="img"
+                    sx={{
+                      height: "50px",
+                      margin: "10px",
+                      marginBottom: "4px",
+                    }}
+                    alt="DeSci Funder"
+                    src="/logo.png"
+                  />
+                </Link>
+              </Grid>
+              <Grid item xs={9}>
+                <Link to="/">
+                  <Typography
+                    color="primary"
+                    variant="h5"
+                    sx={{ fontFamily: "Roboto Condensed", marginBottom: "4px" }}
+                  >
+                    De
+                    <span className="main-title-span">Sci </span>Funder
+                  </Typography>
+                </Link>
+              </Grid>
             </Grid>
           </Grid>
+
+          {/* <Grid item xs = {10} */}
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+
+              {/* <Link to="/about">
+              <Button>About</Button>
+            </Link> */}
+              <Link to="/projects">
+                <Button>Projects</Button>
+              </Link>
+              {auth.scientist ? (
+                <>
+                  <Link to="/addproject">
+                    <Button>Start A Project</Button>
+                  </Link>
+                  <Link to={`/dashboard/${auth.id}`}>
+                    <Button>Dashboard</Button>
+                  </Link>
+                </>
+              ) : null}
+              <Link to={`/user/${auth.id}`}>
+                <Button>My Profile</Button>
+              </Link>
+              <a href="/login" onClick={handleClick}>
+                <Button>Logout</Button>
+              </a>
+              <Tooltip title="My Profile">
+                <Link to={`/user/${auth.id}`}>
+                  <IconButton onClick={handleOpenUserMenu}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src={profileImg}
+                      sx={{ width: 60, height: 60 }}
+                    />
+                  </IconButton>
+                </Link>
+              </Tooltip>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+
+              {/* <Link to="/about">
+              <Button>About</Button>
+            </Link> */}
+              <Link to="/projects">
+                <Button>Projects</Button>
+              </Link>
+              <Link to="/login">
+                <Button>Login</Button>
+              </Link>
+              <Link to="/signup">
+                <Button>Sign Up</Button>
+              </Link>
+            </div>
+          )}
         </Grid>
-
-        {/* <Grid item xs = {10} */}
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-
-            {/* <Link to="/about">
-              <Button>About</Button>
-            </Link> */}
-            <Link to="/projects">
-              <Button>Projects</Button>
-            </Link>
-            {auth.scientist ? (
-              <>
-                <Link to="/addproject">
-                  <Button>Start A Project</Button>
-                </Link>
-                <Link to={`/dashboard/${auth.id}`}>
-                  <Button>Dashboard</Button>
-                </Link>
-              </>
-            ) : null}
-            <Link to={`/user/${auth.id}`}>
-              <Button>My Profile</Button>
-            </Link>
-            <a href="/login" onClick={handleClick}>
-              <Button>Logout</Button>
-            </a>
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-
-            {/* <Link to="/about">
-              <Button>About</Button>
-            </Link> */}
-            <Link to="/projects">
-              <Button>Projects</Button>
-            </Link>
-            <Link to="/login">
-              <Button>Login</Button>
-            </Link>
-            <Link to="/signup">
-              <Button>Sign Up</Button>
-            </Link>
-          </div>
-        )}
-      </Grid>
-    </AppBar>
-  </div>
-);
+      </AppBar>
+    </div>
+  );
+};
 
 /**
  * CONTAINER
@@ -123,6 +145,7 @@ const mapState = (state) => {
   return {
     isLoggedIn: !!state.auth.id,
     auth: state.auth,
+    profileImg: state.auth.profileImg,
   };
 };
 
