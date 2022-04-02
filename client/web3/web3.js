66b6c1fd6db9aa02285267a5145ca9a4770df0e8
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-04-02 14:30:46 -0300

    now page is loading on safari when metamask is not available - validateDOMNesting issue outstanding

--- client/web3/web3.js
+++ client/web3/web3.js
@@ -1,5 +1,5 @@
-import Web3 from 'web3';
-import Campaign from '../../build/contracts/Campaign.json';
+import Web3 from "web3";
+import Campaign from "../../build/contracts/Campaign.json";
 
 //this will initialize a web3 instance with the metamask provider that Metamask injects on the browser
 //"window" variable cannot be accessed using Next.js since Next does server side rendering and
@@ -11,10 +11,20 @@ let currentAccount = null;
 function handleAccountsChanged(accounts) {
   if (accounts.length === 0) {
     // MetaMask is locked or the user has not connected any accounts
-    console.log('Please connect to MetaMask.');
+    console.log("Please connect to MetaMask.");
   } else if (accounts[0] !== currentAccount) {
     currentAccount = accounts[0];
     // Do any other work!
+  }
+}
+
+function handleEthereum() {
+  const { ethereum } = window;
+  if (ethereum && ethereum.isMetaMask) {
+    console.log("Ethereum successfully detected!");
+    // Access the decentralized web!
+  } else {
+    console.log("Please install MetaMask!");
   }
 }
 
@@ -22,29 +32,51 @@ function handleAccountsChanged(accounts) {
 export async function loadWeb3() {
   try {
     if (window.ethereum) {
+      handleEthereum();
       window.web3 = new Web3(window.ethereum);
       const address = await window.ethereum.request({
-        method: 'eth_requestAccounts',
+        method: "eth_requestAccounts",
       });
-      window.ethereum.on('accountsChanged', async () => {
+      window.ethereum.on("accountsChanged", async () => {
         window.location.reload(true);
-      })
+      });
       return address;
     } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
       return true;
     } else {
-      return false;
+      window.addEventListener("ethereum#initialized", handleEthereum, {
+        once: true,
+      });
+
+      // If the event is not dispatched by the end of the timeout,
+      // the user probably doesn't have MetaMask installed.
+      setTimeout(handleEthereum, 3000);
+      
     }
   } catch (error) {
     console.log(error);
   }
 }
 
-window.ethereum.on('accountsChanged', async () => {
-  console.log('accounts changing!')
-  await loadWeb3();
-});
+function accountChanged() {
+  if (window.ethereum) {
+    window.ethereum.on("accountsChanged", async () => {
+      console.log("accounts changing!");
+      await loadWeb3();
+    });
+  } else {
+    window.addEventListener("ethereum#initialized", handleEthereum, {
+      once: true,
+    });
+
+    // If the event is not dispatched by the end of the timeout,
+    // the user probably doesn't have MetaMask installed.
+    setTimeout(handleEthereum, 3000);
+  }
+}
+
+accountChanged();
 
 export async function loadContractData(contractAddress) {
   const web3 = window.web3;
